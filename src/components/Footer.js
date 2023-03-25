a85b586fea4e9d6aca8c70e2adf40a982f803b35
douglasfrancis <dougiefrancis@gmail.com>
2023-03-25 19:23:38 +0000

    SEO title and description added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
     <footer>
       <p>© {new Date().getFullYear()} Woodland Walks Congleton</p>
 
-      <a href='https://firebasestorage.googleapis.com/v0/b/frantech-d30c6.appspot.com/o/terms.pdf?alt=media&token=7a513754-7bd7-43d7-b14d-6a076e15fb5e' target='_blank'>Terms &amp; Conditions</a>
-    
+      {//<a href='https://firebasestorage.googleapis.com/v0/b/frantech-d30c6.appspot.com/o/terms.pdf?alt=media&token=7a513754-7bd7-43d7-b14d-6a076e15fb5e' target='_blank'>Terms &amp; Conditions</a>
+}
     </footer>
   )
 }
