bb4395d1acf65eb0476e9632e7dbd1889a6993cc
douglasfrancis <dougiefrancis@gmail.com>
2023-05-01 15:09:47 +0100

    Update about contents

--- src/components/About.js
+++ src/components/About.js
@@ -10,12 +10,26 @@ export default function About() {
 
         <div className='txt'>
           <h1 style={{color:'#545139'}}>ABOUT</h1>
-          <p >My name is Angelina and I’m the owner of Woodland Dog Walks Congleton.<br/><br/>Based near Timbersbrook, Congleton, we are lucky enough to have Biddulph Valley Way and Bosley cloud on our door step.
-          <br/><br/>Other than having a number of pets (Dogs, Cats & Chickens) I have worked in veterinary practices and doggy day care businesses.<br/><br/>
-          We know how important it is for our precious pooches to get the attention and exercise they deserve.
-          
-           
-          </p>
+      <p>
+        I’m Angelina and I live in Congleton with my husband and two young children - and our Labrador, Bonnie, of course! 
+
+        <br/><br/>
+
+        We live right on Biddulph Valley Way, close to wonderful pastures, brooks, and of course woodland. Each season brings something new and beautiful to discover, so every outing is an adventure! 
+
+        <br/><br/>
+
+        I have experience in veterinary practices, and have previously worked in kennels that provided day care, extended stays and dog walking services. Building on those acquired skills, I am now providing fully insured dog walking and pet sitting services in Congleton and surrounding areas. 
+
+        <br/><br/>
+
+        One of my favourite local walks is Bosley Cloud, which offers amazing views surrounded by heathland once you reach the top!
+
+        <br/><br/>
+
+        I am grateful to combine my love for animals with my love of the outdoors and walking, and sure your dog will have the best time with Woodland Walks Congleton 🐾 </p>
+                  
+       
         </div>
 
           
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,8 +36,7 @@ export default function Contact() {
   return (
     <div id='contact' className='white'>
       <h1>Contact</h1>
-
-      <p>For extended stays and other services we have partnered up with <a style={{fontWeight:'bold', color: '#545139', fontSize: '0.9rem', textDecoration:'underline'}} target='_blank' href='https://uphillndowndale.co.uk/'>Up Hill 'n' Down Dale</a></p>
+      <p>For dog boarding and day care, I have partnered up with the lovely Laura at <a style={{fontWeight:'bold', color: '#545139', fontSize: '0.9rem', textDecoration:'underline'}} target='_blank' href='https://uphillndowndale.co.uk/'>Up Hill 'n' Down Dale</a> based in Buglawton, Congleton. </p>
       <p>Dog walking in Congleton, home visits or all other enquiries, please fill in the form below and we will get back to you as soon as possible.</p>
 
       <form ref={formRef} onSubmit={sendEmail}>
